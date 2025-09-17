edb3cb448373a281e9fdc24ebf7dbf100a749793
Zari777777 <zdogambekova@gmail.com>
2025-09-18 02:05:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post("/ask", async (req, res) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": Bearer ${process.env.OPENAI_API_KEY},
+        "Authorization": 'Bearer ${process.env.OPENAI_API_KEY}',
       },
       body: JSON.stringify({
         model: "gpt-4o-mini",
